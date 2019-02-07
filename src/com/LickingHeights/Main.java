3aaf52bfa9dc8ea962238ffe6d719026b8a2c754
Unknown <connor.mcnutt.2022@student.lhschools.org>
2019-02-07 14:11:52 -0500

    Added the code that decides what the answer is.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -18,6 +18,38 @@ public class Main {
         System.out.println("Type a number between 1-100");
         number = keyboard.nextInt();
 
-        
+        if(number<10) {
+            System.out.println("Yes");
+        }
+        else if(number<20) {
+            System.out.println("No");
+        }
+        else if(number<30) {
+            System.out.println("Outlook good");
+        }
+        else if(number<40) {
+            System.out.println("Outlook not so good");
+        }
+        else if(number<50) {
+            System.out.println("Signs point to yes");
+        }
+        else if(number<60) {
+            System.out.println("Signs point to no");
+        }
+        else if(number<70) {
+            System.out.println("Probably");
+        }
+        else if(number<80) {
+            System.out.println("Probably not");
+        }
+        else if(number<90) {
+            System.out.println("Most likely");
+        }
+        else if(number<100) {
+            System.out.println("Most unlikely");
+        }
+        else {
+            System.out.println("Reply hazy, try again later.");
+        }
     }
 }
