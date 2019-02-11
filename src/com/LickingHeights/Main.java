3c47203f0ce6f9b8a15bac60f03a7d8393320aa0
ConnorM2018 <42775777+ConnorM2018@users.noreply.github.com>
2019-02-11 12:26:11 -0500

    Update Main.java
    Randomized the answers a bit more, and made the program restate the user's question right before giving the answer.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -11,15 +11,22 @@ public class Main {
         String question;
         keyboard = new Scanner(System.in);
 
-        //initialize
+        //Initialize
         System.out.println("Welcome to Magic 8 Ball.");
+        System.out.println("________________________");
         System.out.println("Please ask a question.");
         question = keyboard.nextLine();
-        System.out.println("Type a number between 1-100");
+        System.out.println("Now, type any number between 1-100.");
         number = keyboard.nextInt();
-
+        
+        //The code below is for the response.
+        System.out.println("----------------------------------------");
+        System.out.println("Your question: "+question);
+        System.out.print("Answer: ");
+        
+        //The code below decides what the answer shall be based off the number given by the user.
         if(number<10) {
-            System.out.println("Yes.");
+            System.out.println("No.");
         }
         else if(number<20) {
             System.out.println("Probably.");
@@ -28,28 +35,28 @@ public class Main {
             System.out.println("Outlook good.");
         }
         else if(number<40) {
-            System.out.println("Outlook not so good.");
+            System.out.println("Most unlikely.");
         }
         else if(number<50) {
             System.out.println("Signs point to yes.");
         }
         else if(number<60) {
-            System.out.println("Signs point to no.");
+            System.out.println("Outlook not so good.");
         }
         else if(number<70) {
-            System.out.println("No.");
+            System.out.println("Yes.");
         }
         else if(number<80) {
-            System.out.println("Most unlikely.");
+            System.out.println("Most likely.");
         }
         else if(number<90) {
-            System.out.println("Most likely.");
+            System.out.println("Signs point to no.");
         }
         else if(number<100) {
             System.out.println("Probably not.");
         }
         else {
-            System.out.println("Reply hazy, try again later.");
+            System.out.println("Reply hazy, please try again later.");
         }
     }
 }
